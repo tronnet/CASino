e67aa847b350e028fe2134a03391b6020e44c873
Pedro Henrique <pedrohenriquerls@gmail.com>
2015-04-23 12:21:17 -0300

    silly fix to authenticator

--- app/authenticators/casino/mongoid_authenticator.rb
+++ app/authenticators/casino/mongoid_authenticator.rb
@@ -38,7 +38,7 @@ class CASino::MongoidAuthenticator  < CASino::Authenticator
 
   def extra_attributes(user)
     extra_attributes_option.each_with_object({}) do |(attribute_name, database_column), attributes|
-      value = user[database_column]
+      value = user.extra_attributes[database_column]
       value = value.to_s if value.is_a?(Moped::BSON::ObjectId)
       attributes[attribute_name] = value
     end
